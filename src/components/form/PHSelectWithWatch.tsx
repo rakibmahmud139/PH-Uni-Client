063f349e7b2a173b19d5e8803fc94d95a2bc3648
Rakibmahmud <hasansaikat74@gmail.com>
2024-02-10 11:15:57 +0600

    added offered course

---
+++ src/components/form/PHSelectWithWatch.tsx
@@ -0,0 +1,52 @@
+import { Form, Select } from "antd";
+import { useEffect } from "react";
+import { Controller, useFormContext, useWatch } from "react-hook-form";
+
+type TPHSelectProps = {
+  label: string;
+  name: string;
+  options: { value: string; label: string; Disabled?: boolean }[] | undefined;
+  disabled?: boolean;
+  mode?: "multiple" | undefined;
+  onValueChange: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const PHSelectWithWatch = ({
+  label,
+  name,
+  options,
+  disabled,
+  mode,
+  onValueChange,
+}: TPHSelectProps) => {
+  const { control } = useFormContext();
+  const inputValue = useWatch({
+    control,
+    name,
+  });
+
+  useEffect(() => {
+    onValueChange(inputValue);
+  }, [onValueChange, inputValue]);
+
+  return (
+    <Controller
+      name={name}
+      render={({ field, fieldState: { error } }) => (
+        <Form.Item label={label}>
+          <Select
+            mode={mode}
+            style={{ width: "100%" }}
+            {...field}
+            options={options}
+            size="large"
+            disabled={disabled}
+          />
+          {error && <small style={{ color: "red" }}>{error?.message}</small>}
+        </Form.Item>
+      )}
+    />
+  );
+};
+
+export default PHSelectWithWatch;
--- src/pages/admin/course-Management/OfferCourse.tsx
+++ src/pages/admin/course-Management/OfferCourse.tsx
@@ -1,8 +1,39 @@
+import { Button, Col, Flex } from "antd";
+import PHForm from "../../../components/form/PHForm";
+import PHInput from "../../../components/form/PHInput";
+import PHSelectWithWatch from "../../../components/form/PHSelectWithWatch";
+import { useGetAcademicFacultiesQuery } from "../../../redux/features/admin/academicManagement.api";
+import { useState } from "react";
+import { SubmitHandler, FieldValues } from "react-hook-form";
+
 const OfferCourse = () => {
+  const [id, setId] = useState("");
+  const { data: academicFacultyData } = useGetAcademicFacultiesQuery(undefined);
+
+  const academicFacultyOptions = academicFacultyData?.data?.map((item) => ({
+    value: item._id,
+    label: item.name,
+  }));
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <h1>This is OfferCourse page</h1>
-    </div>
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHSelectWithWatch
+            onValueChange={setId}
+            label="Academic Faculty"
+            name="academicFaculty"
+            options={academicFacultyOptions}
+          />
+          <PHInput disabled={!id} type="text" label="Test" name="test" />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
 
